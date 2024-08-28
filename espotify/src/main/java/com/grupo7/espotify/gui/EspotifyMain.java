aa5a26f529115c264b85e6114d16cce570d66de2
Martín <77687435+LatiosLaw@users.noreply.github.com>
2024-08-28 16:08:45 -0300

    Separacion Inicial de Directorios
    Intimida un poquito menos.

---
+++ espotify/src/main/java/com/grupo7/espotify/gui/EspotifyMain.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.grupo7.espotify.gui;
+
+/**
+ *
+ * @author Law
+ */
+
+import javax.swing.JFrame;  
+
+public class EspotifyMain {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Espotify - Administrator");  
+        frame.setSize(200, 300);  
+        frame.setVisible(true);  
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+    }
+}
